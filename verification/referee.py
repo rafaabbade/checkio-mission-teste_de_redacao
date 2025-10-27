7180ca0ad6f4d0cfaad11f679ac2ed50b12b2ad7
Rafael Abbade <rafaelvabbade@gmail.com>
2025-10-27 11:03:11 -0300

    Initial commit from template

--- verification/referee.py
+++ verification/referee.py
@@ -38,7 +38,7 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "{{FUNC}}",
+            "python": "verificar_redacao",
             "js": "countConsecutiveSummers"
         },
         cover_code={
--- verification/tests.py
+++ verification/tests.py
@@ -10,6 +10,55 @@ Each test is a dict with
 
 TESTS = {
     "Basics": [
-        {{TESTES}}
+        [
+    {
+        "input": [2, 10, ["melhor", "que", "machado", "de", "assis"]],
+        "answer": "OK"
+    },
+    {
+        "input": [6, 6, ["b", "c", "d", "e", "f", "g"]],
+        "answer": "OK"
+    },
+    {
+        "input": [6, 8, ["aa", "bb", "aa", "bb", "aa", "bb"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [3, 10, ["vida", "2vida", "real"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [3, 10, ["vida", "real_mente", "ok"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [3, 10, ["vida", "Real", "ok"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [3, 10, ["aaaaaaaaaaa", "b", "c"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [3, 10, ["", "vida", "real"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [4, 10, ["vida", "real", "boa"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [5, 10, ["palavra", "palavra", "palavra", "palavra", "palavra"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [4, 10, ["abc", "def", "ghi", "jkl"]],
+        "answer": "Inválido"
+    },
+    {
+        "input": [4, 10, ["abc", "def", "ghi", "jkl", "mno", "pqr"]],
+        "answer": "OK"
+    }
+]
     ]
 }
